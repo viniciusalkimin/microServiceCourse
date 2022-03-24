b3a5ef457652eebae6f476f7cb1fd2425c31287d
Vinicius Goncalves Alkimin <vinicius.alkimin@solutis.net.br>
2022-03-24 17:27:34 -0300

    configuring Eureka server

--- hr-eureka-server/src/main/java/com/devsuperior/hreurekaserver/HrEurekaServerApplication.java
+++ hr-eureka-server/src/main/java/com/devsuperior/hreurekaserver/HrEurekaServerApplication.java
@@ -2,12 +2,14 @@ package com.devsuperior.hreurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-public class HrEurekaserverApplication {
+@EnableEurekaServer
+public class HrEurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HrEurekaserverApplication.class, args);
+		SpringApplication.run(HrEurekaServerApplication.class, args);
 	}
 
 }
